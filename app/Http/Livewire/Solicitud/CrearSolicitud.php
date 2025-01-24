a03304d7a43ca3b03cabae7682897db31fe822fc
DAVID PC-CAJA <davidortiz25122010@gmail.com>
2025-01-24 13:56:10 -0400

    CAMBIOS

--- app/Http/Livewire/Solicitud/CrearSolicitud.php
+++ app/Http/Livewire/Solicitud/CrearSolicitud.php
@@ -6,8 +6,31 @@ use Livewire\Component;
 
 class CrearSolicitud extends Component
 {
+
+    public $ver_datos, $documento, $nombre, $apellido, $email, $direccion, $celular;
+    public $ver_prestamos;
+
+    public function mount()
+    {
+        $this->ver_datos = 'block';
+        $this->ver_prestamos = 'none';
+    }
+
     public function render()
     {
         return view('livewire.solicitud.crear-solicitud');
     }
+
+    public function detalle_prestamos_click()
+    {
+        $this->ver_datos = 'none';
+        $this->ver_prestamos = 'block';
+    }
+
+    public function retro_ver_datos()
+    {
+        $this->ver_datos = 'block';
+        $this->ver_prestamos = 'none';
+    }
+
 }
--- resources/views/livewire/solicitud/crear-solicitud.blade.php
+++ resources/views/livewire/solicitud/crear-solicitud.blade.php
@@ -1,6 +1,6 @@
 <div class="col-lg-12 col-12 layout-spacing">
 
-    <div class="widget-content widget-content-area">
+    <div class="widget-content widget-content-area" style="display: {{$ver_datos}}">
         <div class="widget widget-table-two">
             <div class="widget-heading">
                 <h5 class="">Datos Personales</h5>
@@ -10,7 +10,7 @@
                 <div class="form-row">
                     <div class="col-md-3 mb-4">
                         <label for="documento">Documento</label>
-                        <input type="text" wire:model.defer="documento" class="form-control text-right" placeholder="Documento" 
+                        <input type="text" wire:model.defer="documento" class="form-control text-right" placeholder="Documento"
                         onchange="puntos_decimal(this)" onkeyup="puntos_decimal(this)">
                     </div>
                     <div class="col-md-3 mb-4">
@@ -34,10 +34,53 @@
                         <input type="text" wire:model.defer="celular" class="form-control" placeholder="Celular">
                     </div>
                 </div>
+
+                <div class="form-row">
+                    <button type="button" class="btn btn-info" wire:click="detalle_prestamos_click()">Continuar</button>
+                </div>
             </div>
         </div>
 
-        
+
+    </div>
+
+    <div class="widget-content widget-content-area"  style="display: {{$ver_prestamos}}">
+        <div class="widget widget-table-two">
+            <div class="widget-heading">
+                <h5 class="">Detalles del Prestamos</h5>
+            </div>
+
+            <div class="widget-content">
+                <div class="form-row">
+                    <div class="col-md-12 mb-4">
+                        <label for="persona">Persona</label>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="col-md-3 mb-4">
+                    <label for="monto_solicitado">Monto Solicitado</label>
+                    <input type="text" wire:model.defer="monto_solicitado" class="form-control text-right" placeholder="monto_solicitado"
+                    onchange="puntos_decimal(this)" onkeyup="puntos_decimal(this)">
+                </div>
+                <div class="form-row">
+                    <div class="col-md-3 mb-4">
+                        <label for="plazo">Plazo</label>
+                        <select name="plazo" id="plazo" class="form-control basic">
+                            @for ($i = 1; $i < 36; $i++)
+                                <option value="{{$i}}">{{$i}}</option>
+                            @endfor
+                        </select>
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <button type="button" class="btn btn-warning mr-2" wire:click="retro_ver_datos()">Retroceder</button>
+                    <button type="button" class="btn btn-info">Continuar</button>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 
 </div>
